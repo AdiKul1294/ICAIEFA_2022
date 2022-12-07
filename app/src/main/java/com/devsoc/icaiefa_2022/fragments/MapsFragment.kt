4a93882671c72cb09001b3a4ae649aad644247f9
AdiKul1294 <f20211294@goa.bits-pilani.ac.in>
2022-12-07 19:59:55 +0530

    Added Maps and Visiting Places

--- app/src/main/java/com/devsoc/icaiefa_2022/fragments/MapsFragment.kt
+++ app/src/main/java/com/devsoc/icaiefa_2022/fragments/MapsFragment.kt
@@ -16,21 +16,33 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
     private val callback = OnMapReadyCallback { googleMap ->
-        with(googleMap.uiSettings) { isZoomControlsEnabled = true
+        with(googleMap.uiSettings) {
+            isZoomControlsEnabled = true
             isCompassEnabled = true
         }
         val markers: List<MarkerOptions> = listOf(
-            MarkerOptions().position(LatLng(15.390725144730112, 73.87846601024137)).title("BITS Goa").snippet("Welcome to BITS Goa!"),
+            MarkerOptions().position(LatLng(15.390725144730112, 73.87846601024137))
+                .title("BITS Goa").snippet("Welcome to BITS Goa!"),
             MarkerOptions().position(LatLng(15.393059810098007, 73.88400429732732)).title("D Mess"),
-                MarkerOptions().position(LatLng(15.388409935353897, 73.87679557050468)).title("BITS Visitor's Guest House"),
-            MarkerOptions().position(LatLng(15.391634129693461, 73.87607826751572)).title("Medical Center, BITS Goa"),
+            MarkerOptions().position(LatLng(15.388409935353897, 73.87679557050468))
+                .title("BITS Visitor's Guest House"),
+            MarkerOptions().position(LatLng(15.391634129693461, 73.87607826751572))
+                .title("Medical Center, BITS Goa"),
             MarkerOptions().position(LatLng(15.392091481206723, 73.88179633166733)).title("DLT-10")
         )
         markers.forEach {
             googleMap.addMarker(it)
         }
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(15.390725144730112, 73.87846601024137), 15.5f))
+        googleMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    15.390725144730112,
+                    73.87846601024137
+                ), 15.5f
+            )
+        )
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -46,9 +58,11 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
 
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
 //    private fun addMarkers(googleMap: GoogleMap) {
