22cb409a2a6cb4a4d5f26a49bdd414e997328a87
mmjsmohit <mmjsmohit@gmail.com>
2022-12-07 19:31:32 +0530

    Added Google Map and markers for notable locations.

--- app/src/main/java/com/devsoc/icaiefa_2022/fragments/MapsFragment.kt
+++ app/src/main/java/com/devsoc/icaiefa_2022/fragments/MapsFragment.kt
@@ -6,8 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.devsoc.icaiefa_2022.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), OnMapReadyCallback {
+    private val callback = OnMapReadyCallback { googleMap ->
+        with(googleMap.uiSettings) { isZoomControlsEnabled = true
+            isCompassEnabled = true
+        }
+        val markers: List<MarkerOptions> = listOf(
+            MarkerOptions().position(LatLng(15.390725144730112, 73.87846601024137)).title("BITS Goa").snippet("Welcome to BITS Goa!"),
+            MarkerOptions().position(LatLng(15.393059810098007, 73.88400429732732)).title("D Mess"),
+                MarkerOptions().position(LatLng(15.388409935353897, 73.87679557050468)).title("BITS Visitor's Guest House"),
+            MarkerOptions().position(LatLng(15.391634129693461, 73.87607826751572)).title("Medical Center, BITS Goa"),
+            MarkerOptions().position(LatLng(15.392091481206723, 73.88179633166733)).title("DLT-10")
+        )
+        markers.forEach {
+            googleMap.addMarker(it)
+        }
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(15.390725144730112, 73.87846601024137), 15.5f))
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -19,5 +42,16 @@ class MapsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+//    private fun addMarkers(googleMap: GoogleMap) {
+//    }
 
 }
