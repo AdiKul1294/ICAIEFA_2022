65759b4b9832c716bcea05e75755d2cd89385659
AdiKul1294 <f20211294@goa.bits-pilani.ac.in>
2022-12-08 15:37:10 +0530

    added visiting places and changes to font

--- app/src/main/java/com/devsoc/icaiefa_2022/fragments/VisitingPlacesFragment.kt
+++ app/src/main/java/com/devsoc/icaiefa_2022/fragments/VisitingPlacesFragment.kt
@@ -30,6 +30,30 @@ class VisitingPlacesFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_visiting_places, container, false)
 
+        binding.place1CvVpf.setOnClickListener {
+            val builder : CustomTabsIntent.Builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse("https://maps.app.goo.gl/JJqAywWz2VWoBd1f8"))
+        }
+
+        binding.place2CvVpf.setOnClickListener {
+            val builder : CustomTabsIntent.Builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse("https://maps.app.goo.gl/LxWXZ25HzAuUxjgYA"))
+        }
+
+        binding.place3CvVpf.setOnClickListener {
+            val builder : CustomTabsIntent.Builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse("https://maps.app.goo.gl/wbmejUsFEqv1Sg9r8"))
+        }
+
+        binding.place4CvVpf.setOnClickListener {
+            val builder : CustomTabsIntent.Builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(requireContext(), Uri.parse("https://maps.app.goo.gl/Y6qJzBAJ1wm569Bu9"))
+        }
+
         var placesList = mutableListOf<Place>(
             Place(
                 "Bogmalo Beach",
@@ -37,19 +61,38 @@ class VisitingPlacesFragment : Fragment() {
                 "https://maps.app.goo.gl/N7a73BLJ5x5wmbQ56"
             ),
             Place(
-                "Bogmalo Beach",
-                R.mipmap.bogmalo,
-                "https://maps.app.goo.gl/N7a73BLJ5x5wmbQ56"
+                "Baga Beach",
+                R.mipmap.baga,
+                "https://maps.app.goo.gl/UGrw3chxCjvz56mUA"
             ),
             Place(
-                "Bogmalo Beach",
-                R.mipmap.bogmalo,
-                "https://maps.app.goo.gl/N7a73BLJ5x5wmbQ56"
+                "Majorda Beach",
+                R.mipmap.majorda,
+                "https://maps.app.goo.gl/ic4zmvM57TnWJ9DK6"
             ),
             Place(
-                "Bogmalo Beach",
-                R.mipmap.bogmalo,
-                "https://maps.app.goo.gl/N7a73BLJ5x5wmbQ56"
+                "Sunset Cruise",
+                R.mipmap.cruise,
+                "https://maps.app.goo.gl/oNdkY2baHwrjTXyHA"
+            ),
+            Place(
+                "Shantadurga Temple",
+                R.mipmap.shantadurga,
+                "https://maps.app.goo.gl/rNiUentyJM6nnd9u5"
+            ),
+            Place("Old Goa Church",
+                R.mipmap.old_goa_church,
+                "https://maps.app.goo.gl/3bZ6GJkgKbq78n779"
+            ),
+            Place(
+                "Spice Farm",
+                R.mipmap.spice_farm,
+                "https://maps.app.goo.gl/r4t2mLLXkxwfKuwH7"
+            ),
+            Place(
+                "Big Foot Museum",
+                R.mipmap.museum,
+                "https://maps.app.goo.gl/52jqjd56pzXB5brD6"
             )
         )
 
